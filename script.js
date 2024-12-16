876e83667c40712332f85e9406d6138630fbbb53
Abdullah khan <akhan283785@gmail.com>
2024-12-16 12:23:38 +0530

    try

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
-    //Write your code here
+  //Write your code here
+  let strong = document.querySelectorAll("strong");
 
-
+  for (let t of strong) {
+    t.style.color = "green";
+  }
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+  //Write your code here
+  let strong = document.querySelectorAll("strong");
+  for (let t of strong) {
+    t.style.color = "initial";
+  }
 }
